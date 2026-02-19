9b0024bd1a9949b199c416bf6b959c850af4e9fb
刘皓 <whiteaxe@tuta.io>
2026-02-19 15:38:46 -0500

    Allocate 4 custom SDL events for the journal instead of 3

--- journal/SDL_custom_events.h
+++ journal/SDL_custom_events.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <SDL3/SDL.h>
-inline const Uint32 user_event_start = [] { return SDL_RegisterEvents(3); }();
+inline const Uint32 user_event_start = [] { return SDL_RegisterEvents(4); }();
 inline const Uint32 ONESHOT_LAUNCHED = [] { return user_event_start + 0; }();
 inline const Uint32 JOURNAL_SET_IMAGE = [] { return user_event_start + 1; }();
 inline const Uint32 JOURNAL_SET_WINDOW_POSITION = [] { return user_event_start + 2; }();
